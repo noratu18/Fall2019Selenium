6dc5cb17326c84e742ff5751e3ded026156a53e5
noratu18 <noratukesh@gmail.com>
2020-03-01 17:37:55 -0500

    Google Search Test

---
+++ src/test/java/com/automation/tests/day2/GoogleSearchTest.java
@@ -0,0 +1,31 @@
+package com.automation.tests.day2;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleSearchTest {
+
+    public static void main(String[] args) throws Exception{
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://google.com");
+        Thread.sleep(2000);
+        //(By.name("q")) --> name ="q"
+        WebElement search = driver.findElement(By.name("q"));
+        //once we found an element, how to enter text?
+        //to enter text, use senKLeys() method
+        // how to press enterr after entering the text?
+        //use Key.ENTER
+        //Keys.ENTER - perform keyboard click
+        search.sendKeys("Java", Keys.ENTER);
+        Thread.sleep(2000);
+        driver.quit();
+
+
+    }
+}

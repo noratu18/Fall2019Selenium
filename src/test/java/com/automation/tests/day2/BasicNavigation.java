3616cb3e3b6b8d6ce26b2b3c24e4cc286f8e0737
noratu18 <noratukesh@gmail.com>
2020-03-01 15:46:29 -0500

    created basic navigation class

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -15,13 +15,15 @@ public class BasicNavigation {
         // In selenium, everything starts from WebDriver interface
         // ChromeDriver extends RemoteWebDriver--> implements WebDriver
         driver.get("http://google.com");//to open a website
+        driver.manage().window().maximize(); // to maximize browser
+     // driver.manage().window().fullscreen(); // open browser in full screen
 
         Thread.sleep(3000);// for demo, wait 3 seconds
 
         // method that returns page title
         // you can also see it as tab name, in the browser
         String title = driver.getTitle();// returns <title> Some title</title> text
-        String expectedTitle = "Google";
+        String expectedTitle = "Google"; // we provide it
 
         System.out.println("title = " + title);
 
@@ -31,14 +33,55 @@ public class BasicNavigation {
             System.out.println("Test failed");
         }
 
+        // go to another website within the same window
+        driver.navigate().to("http://amazon.com");
 
+        if(driver.getTitle().toLowerCase().contains("amazon")){
+            System.out.println("Test passed");
+        }else{
+            System.out.println("Test failed");
+        }
+
+        // comeback to google
+        driver.navigate().back();
+        Thread.sleep(3000);
+        //checking if page title is equals to Google
+        //.getTitle() - returns page title
+        verifyEquals(driver.getTitle(),"Google");
+        // move forward in the browser history
+        driver.navigate().forward();
+        Thread.sleep(3000);
+        System.out.println("Title: "+ driver.getTitle());
+        // driver.getTitle() - returns page title of the page that is currently opened
+
+        // to get URL
+        System.out.println("URL: "+driver.getCurrentUrl());
+
+        driver.navigate().refresh();//to reload page
+        Thread.sleep(3000);
+
+        // must be at the end
         driver.close(); // to close browser
         // browser cannot close itself
 
+    }
 
+    /**
+     * Check if two strings are same. If print TEST PASSED!! message
+     * Otherwise, print TEST FAILED message
+     * @param arg1
+     * @param arg2
+     */
+    public static void verifyEquals(String arg1, String arg2){
 
-
+        if(arg1.equals(arg2)){
+            System.out.println("Test passed");
+        }else{
+            System.out.println("Test failed");
+        }
 
 
     }
+
+
 }

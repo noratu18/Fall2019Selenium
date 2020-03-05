2a6619db2dd9320f026ea05383fd6d4eb9a9f725
noratu18 <noratukesh@gmail.com>
2020-03-05 10:38:50 -0500

    My First Selenium update

--- src/test/java/com/automation/tests/day1/MyFirstSeleniumScript.java
+++ src/test/java/com/automation/tests/day1/MyFirstSeleniumScript.java
@@ -4,13 +4,43 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MyFirstSeleniumScript {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException{
+
+        // binary the driver abd browsers
         // setup chromedriver
         WebDriverManager.chromedriver().setup();
-        //create chromedriver object
+
+        //create chrome driver object
+        //WebDriver object --> Interface
+        // I have a driver
         ChromeDriver driver = new ChromeDriver();
+
         // open some website
+        //I want to open Google home page
+        // How do you launch/open a web page?
+        //by using get()--> url as String
         driver.get("http://google.com");
+
+        //Navigations
+        //navigate().to() --> open a web page
+
+        // driver.navigate().to("http://www.google.com");
+
+        //get() vs navigate().to()
+        //get() --> wait to load the page
+        //to() --> does no wait
+
+        //navigate().back()-->
+        //1. go to google
+        //2. go to cybertek practice website
+
+        driver.get("https://www.google.com");
+        Thread.sleep(2000);;
+        driver.navigate().to("http://practice.cybertekschool.com/");
+        Thread.sleep(3000);
+        driver.navigate().back();
+
+        driver.close();
 
 
     }

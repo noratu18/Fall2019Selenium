3246c19a30afba2da37317236921ef3e1dbe1838
noratu18 <noratukesh@gmail.com>
2020-03-14 15:47:04 -0400

    Search Tests update

--- src/test/java/com/automation/tests/day8/SearchTests.java
+++ src/test/java/com/automation/tests/day8/SearchTests.java
@@ -31,10 +31,54 @@ public class SearchTests {
             if(!var.isEmpty()){
                 System.out.println(var);
                 //verify that every search result contains java
+                //if some of the search
+                //doesn't contain java word, it will fail the test
                 Assert.assertTrue(var.toLowerCase().contains("java"));
             }
            // System.out.println(searchItem.getText());
         }
+    }
+    /*
+    Given user is on the amazon.com page
+    When user enters "java" as a search item
+    //Then user clicks on the search item
+    //And user clicks on the first search item
+    //And user verifies that title of the search item contains "java"
+     */
+
+    @Test(description = "Search for Java book on amazon")
+    public void amazonSearchTest(){
+        driver.get("http://amazon.com");
+        //there bis a chance that item is not visible
+        //so we need to maximize the window before clicking on it
+        driver.manage().window().maximize();
+
+        BrowserUtils.wait(5);
+
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Java",Keys.ENTER);
+        BrowserUtils.wait(5);
+
+        //find all links inside h2 elements, beacuse he elements is not clicklable itself
+        List<WebElement> searchItems = driver.findElements(By.xpath("//h2//a"));
+
+        //click on the first item
+        for(WebElement searchItem : searchItems){
+            System.out.println("Title: " + searchItem.getText());
+        }
+        searchItems.get(0).click();
+        BrowserUtils.wait(5);
+
+        WebElement productTitle = driver.findElement(By.id("title"));
+        String productTitleString = productTitle.getText();
+        System.out.println(productTitleString);
+
+        Assert.assertTrue(productTitleString.contains("Java"));;
+
+        //so h2 elements are not clickable, even though they contain links
+        //that's why , instead off collelction all h2 elements
+        //we collected all hyperlinks
+        //everry hyperlink represent some search item
+
     }
 
     @BeforeMethod

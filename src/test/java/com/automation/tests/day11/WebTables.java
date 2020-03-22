c5beeff5256510db1c5452f9509b6153aa69785e
noratu18 <noratukesh@gmail.com>
2020-03-22 11:21:37 -0400

    WebTables

---
+++ src/test/java/com/automation/tests/day11/WebTables.java
@@ -0,0 +1,147 @@
+package com.automation.tests.day11;
+
+import com.automation.utilities.BrowserUtils;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class WebTables {
+
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setup(){
+       // driver = DriverFactory.createDriver("chrome");
+        WebDriverManager.chromedriver().version("79").setup();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        //headless mode makes executio twice faster
+        // it does everything except file uploading
+        //set it to true to make it work
+        chromeOptions.setHeadless(true);//to run browser without GUI, makes browser invisible
+        driver = new ChromeDriver(chromeOptions);
+        driver.get("http://practice.cybertekschool.com/tables");
+        driver.manage().window().maximize();
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            BrowserUtils.wait(2);
+            driver.quit();
+            driver = null;
+        }
+    }
+
+    @Test
+    public void getColumnNames(){
+        //th -> represents table header cells
+        List<String> expected = Arrays.asList("Last Name","First Name","Email","Due","Web Site","Action");
+        List<WebElement> columnNames=driver.findElements(By.xpath("//table[1]//th"));
+
+        //just to print
+        for(WebElement columnName : columnNames){
+            System.out.println(columnName.getText());
+
+        }
+        Assert.assertEquals(BrowserUtils.getTextFromWebElements(columnNames),expected);
+
+    }
+
+    @Test
+    public void verifyRowCount(){
+        // tbody//tr --> to get all rows from table body,  excluding tabke header
+        List<WebElement> rows = driver.findElements(By.xpath("//table[1]//tbody//tr"));
+        //if we will get a size of this collection, it automatically equals to number of elements
+        //expected --> 4 rows in the table
+        //actual result --> collection(rows.size())
+        Assert.assertEquals(rows.size(),4);
+    }
+
+    /**
+     * To get specific column, skip row index, and just provide td index
+     */
+    @Test
+    public void getSpecificColumn(){
+        //td[5] --> column with links
+        List<WebElement> links = driver.findElements(By.xpath("//table[1]//tbody//tr//td[5]"));
+        System.out.println(BrowserUtils.getTextFromWebElements(links));
+
+
+    }
+
+    //once you find email cell in the first table that has this email
+    //jdoe@hotmail.com then go to following sibling has linkText delete:
+    // //td[text() = 'jdoe@hotmail.com']//following-sibling::td/a[text()='delete']
+
+    //Go to tables example page
+    // Delete record with jsmith@gmail.com email
+    // verify that number of rows is equals to 3
+    //verify that jsmith@gmail.com doesn't exist anymore in the table
+
+    @Test
+    public void deleteEmail(){
+
+        String xpath = "//table[1]//td[text()='jsmith@gmail.com']/..//a[text()='delete']";
+        driver.findElement(By.xpath(xpath)).click();
+        int rowCount = driver.findElements(By.xpath("//table[1]//tbody//tr")).size();
+        Assert.assertEquals(rowCount,3);
+
+        // get count of rows
+        List<WebElement> emails = driver.findElements(By.xpath("/table[1]//td[text()='jsmith@gmail.com']"));
+        Assert.assertTrue(emails.isEmpty());
+
+    }
+
+    /*
+    Let's write a function that will return column index
+    based on column name
+     */
+    @Test
+    public void getColumnIndexByName(){
+        String columnName = "Email";
+
+        List<WebElement> columnNames = driver.findElements(By.xpath("//table[2]//th"));
+
+        int index = 0;
+        for(int i=0;i<columnNames.size();i++){
+            String actualColumnName = columnNames.get(i).getText();
+            System.out.println(String.format("column name: %s, position %s", actualColumnName, i));
+
+            if(actualColumnName.equals(columnName)){
+                index =i+1;
+                break;
+            }
+        }
+
+        Assert.assertEquals(index,3);
+    }
+
+    @Test
+    public void getSpecificCell(){
+
+        String expected = "http://www.jdoe.com";
+
+        int row = 3;
+        int column = 5;
+        String xpath = "//table[1]//tbody//tr["+row+"]//td["+column+"]";
+
+        WebElement cell = driver.findElement(By.xpath(xpath));
+
+        Assert.assertEquals(cell.getText(), expected);
+
+    }
+
+
+}

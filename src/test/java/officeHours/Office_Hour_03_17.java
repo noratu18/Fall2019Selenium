95b32dffefb4b9002ed1316957a753ebfe57725b
noratu18 <noratukesh@gmail.com>
2020-03-25 00:13:20 -0400

    Office hour 03_17 update

--- src/test/java/officeHours/Office_Hour_03_17.java
+++ src/test/java/officeHours/Office_Hour_03_17.java
@@ -7,14 +7,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.internal.WebElementToJsonConverter;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 
 import java.util.HashMap;
 
 public class Office_Hour_03_17 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
 
 
     WebDriver driver = DriverFactory.createDriver("chrome");
+    driver.manage().window().maximize();
+
     driver.get("http://qa3.vytrack.com");
         WebElement username = driver.findElement(By.id("prependedInput"));
         username.sendKeys("salesmanager110");
@@ -32,6 +35,13 @@ public class Office_Hour_03_17 {
         WebElement contact_link = driver.findElement(By.xpath("//span[.='Contacts']/following-sibling::a"));
         BrowserUtils.wait(3);
 
+        contact_link.click();
+        BrowserUtils.wait(3);
+
+        WebElement create_contact = driver.findElement(By.linkText("Create Contact"));
+        create_contact.click();
+        BrowserUtils.wait(3);
+
         String currentTitle = driver.getTitle();
         if(currentTitle.equalsIgnoreCase("Create Contact - Contacts - Customers")){
             System.out.println("Title is expected");
@@ -40,13 +50,14 @@ public class Office_Hour_03_17 {
         }
         HashMap<String,String> contact1 = new HashMap<>();
         contact1.put("First Name", "John");
-        contact1.put("Last name", "Smith");
+        contact1.put("Last Name", "Smith");
         contact1.put("Phone", "517-236-4567");
         contact1.put("Street", "400 Main Street");
         contact1.put("City", "Tysons");
+        contact1.put("State", "VA");
         contact1.put("Zip Code", "22102");
         contact1.put("Sales Group", "true");
-        contact1.put("Country", "United States");
+        contact1.put("Country", "US");
 
         System.out.println("contact1 = " + contact1);
 
@@ -55,7 +66,7 @@ public class Office_Hour_03_17 {
          WebElement phone = driver.findElement(By.name("oro_contact_form[phones][0][phone]"));
          WebElement street = driver.findElement(By.name("oro_contact_form[addresses][0][street]"));
          WebElement city = driver.findElement(By.name("oro_contact_form[addresses][0][city]"));
-         WebElement state = driver.findElement(By.xpath("//input[@data-name = 'field__region-text']"));
+
          WebElement zipCode = driver.findElement(By.xpath("//input[@data-name = 'field__postal-code']"));
          WebElement salesGroup = driver.findElement(By.xpath("(//input[@data-name = 'field__1'])[2]"));
 
@@ -64,7 +75,7 @@ public class Office_Hour_03_17 {
          phone.sendKeys(contact1.get("Phone"));
          street.sendKeys(contact1.get("Street"));
          city.sendKeys(contact1.get("City"));
-         state.sendKeys(contact1.get("State"));
+      //   state.sendKeys(contact1.get("State"));
          zipCode.sendKeys(contact1.get("Zip Code"));
 
          /*
@@ -83,9 +94,43 @@ public class Office_Hour_03_17 {
         it has different methods that help us interact with dropdown
          */
         country_dropdown.selectByVisibleText(contact1.get("Country"));//to make it dynamic we choose country
+
+        WebElement state = driver.findElement(By.xpath("//select[@data-name = 'field__region']"));
+        Select state_list = new Select(state);
+        state_list.selectByVisibleText(contact1.get("State"));
+
         if(contact1.get("Sales Group").equalsIgnoreCase("true")){
             salesGroup.click();
         }
+
+        driver.findElement(By.xpath("(//button[contains(text(),'Save and Close')])[1]")).click();
+
+        Thread.sleep(3000);
+
+        String fullName = contact1.get("First Name")+ " " + contact1.get("Last Name");
+        String uiFullName = driver.findElement(By.xpath("//h1[@class='user-name']")).getText();
+        Assert.assertEquals(uiFullName, fullName);
+
+        System.out.println("Actual: " + uiFullName + " | Expected: " + fullName);
+
+        String uiPhone = driver.findElement(By.className("phone")).getText();
+
+        Assert.assertEquals(uiPhone, contact1.get("Phone"));
+        System.out.println("Actual: " + uiFullName + " | Expected: " + contact1.get("Phone"));
+
+        String uiCompleteAddress = driver.findElement(By.xpath("//address")).getText();
+
+        String cityWithState = (contact1.get("City") + " " + contact1.get("State")+
+                                " " + contact1.get("Country") + " " + contact1.get("Zip Code")).toUpperCase();
+
+        String completeAddress = contact1.get("Street") + "\n" + cityWithState;
+
+        Assert.assertEquals(uiCompleteAddress, completeAddress);
+
+
+
+
+
 
 
 
